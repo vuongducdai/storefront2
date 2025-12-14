5e40b44939b8f91c840a7220aa4aa6f7571b9908
daivd <daivuong7696@gmail.com>
2025-12-14 10:22:45 +0700

    modify database

--- storefront/settings.py
+++ storefront/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront2',
         'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword'
     }
 }
 
