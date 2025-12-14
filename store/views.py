950e781b5a55a89f05621e2b81f43b01d4dde95d
daivd <daivuong7696@gmail.com>
2025-12-14 11:11:21 +0700

    serializers

--- store/views.py
+++ store/views.py
@@ -1,11 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def product_list(request):
-    return Response('ok')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
