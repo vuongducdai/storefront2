704e5bb5635c55365b975de745da8618deca0e15
daivd <daivuong7696@gmail.com>
2025-12-14 12:03:19 +0700

    remove price

--- store/serializers.py
+++ store/serializers.py
@@ -9,12 +9,11 @@ class CollectionSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
     collection = CollectionSerializer()
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal('1.1')
