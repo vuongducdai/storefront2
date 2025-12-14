ba580343a89e8a67fcce50967013a9df5808f6d8
daivd <daivuong7696@gmail.com>
2025-12-14 16:01:55 +0700

    Add collections paths

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product_list'),
     path('products/<int:id>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('collections/', views.CollectionList.as_view(), name='collection_list'),
+    path('collections/<int:id>/', views.CollectionDetail.as_view(), name='collection_detail'),
 ]
