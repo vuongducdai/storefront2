810b91d5ecc03fc2a6b0b205e9f373e299862d75
daivd <daivuong7696@gmail.com>
2025-12-15 07:50:05 +0700

    Use simple router

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('collections/', views.CollectionList.as_view(), name='collection_list'),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection_detail'),
-]
+router = SimpleRouter()
+router.register(r'products', views.ProductViewSet)
+router.register(r'collections', views.CollectionViewSet)
+
+urlpatterns = router
