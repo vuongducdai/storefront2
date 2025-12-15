cc85237941e0ac38cdd2a18f917f92a79f0e4d37
daivd <daivuong7696@gmail.com>
2025-12-15 13:11:45 +0700

    Fix import

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Product, Collection, OrderItems
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
@@ -16,7 +16,7 @@ class ProductViewSet(ModelViewSet):
         return {'request': self.request} 
 
     def destroy(self, request, *args, **kwargs):
-        if OrderItems.objects.filter(product_id=kwargs['pk']).exists():
+        if OrderItem.objects.filter(product_id=kwargs['pk']).exists():
             return Response({'error': 'Product cannot be deleted because it is in use'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
 
