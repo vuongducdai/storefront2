087d8d2505e7a0d7630be93f66a23ed133412103
daivd <daivuong7696@gmail.com>
2025-12-15 07:48:18 +0700

    Convert to viewset

--- store/views.py
+++ store/views.py
@@ -3,32 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Product, Collection
+from .models import Product, Collection, OrderItems
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models import Count
+from rest_framework.viewsets import ModelViewSet
 
-class ProductList(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
 
     def get_serialize_context(self):
         return {'request': self.request} 
 
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItems.objects.filter(product_id=kwargs['pk']).exists():
             return Response({'error': 'Product cannot be deleted because it is in use'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    serializer_class = CollectionSerializer
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
